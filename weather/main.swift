6c487650c3d64306ac36b247ccfa224bb6582f62
GarenLiang <ideabreeze@gmail.com>
2017-05-10 21:07:14 -0700

    mac weather app

---
+++ weather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  weather
+//
+//  Created by GarenLiang on 2017/5/10.
+//  Copyright © 2017年 GarenLiang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
